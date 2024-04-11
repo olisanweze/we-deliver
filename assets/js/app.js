c17332697fc4fdfe0ce6c0108dc54abaec720e75
Olisa Nweze <nwezeolisa@gmail.com>
2024-04-10 21:19:23 -0500

    website style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,68 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*                                                       */
-/*  Andre Specht (2024)                                  */
-/*  github.com/mrspecht                                  */
+/*  Olisa Nweze (2024)                                   */
+/*  github.com/olisanweze                                */
 /*                                                       */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 'use strict';
 
-// This app requires a server to handle import statements and CORS issues
-import * as utils from './utils.js';
-
+import { select, listen } from './utils.js';
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Organizer                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-utils.print('Welcome');
+const track = select('.track-button');
+const placeholder = select('.map-placeholder');
+const map = select('#map');
+
+//  The 'success' callback function
+function getLocation(position) {
+  const { latitude, longitude } = position.coords;
+
+  mapBuild(longitude, latitude);
+}
+
+//  The 'error/failure' callback function
+function errorHandler() {
+  mapBuild(0, 0);
+}
+
+const options = {
+  enableHighAccuracy: true
+}
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2xpc2Fud2V6ZSIsImEiOiJjbHV1aWhmcWQwYWsxMnFvdjE2aHFsa3ZjIn0.gyU5LVBd-DSepVIOzD1LTA';
+
+function mapBuild(longitude, latitude) {
+  const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  center: [longitude, latitude], // starting position [lng, lat]
+  zoom: 10, // starting zoom
+  });
+
+  map.addControl(
+    new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+  );
+  const marker1 = new mapboxgl.Marker({color: '#ff7342'})
+  .setLngLat([longitude, latitude])
+  .addTo(map);
+}
+
+function genMap() {
+  if ('geolocation' in navigator) {
+   // placeholder.innerText ='';
+    navigator.geolocation.getCurrentPosition(
+      getLocation, errorHandler, options
+    );
+  }
+
+  // placeholder.classList.add('.none');
+  map.classList.add('block');
+}
+
+listen('click', track, genMap);
